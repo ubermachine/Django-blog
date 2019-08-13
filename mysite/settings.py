3800a2d9625ec6e359e4d1419f27835664e72c95
Ubermachine <grademy9@gmail.com>
2019-08-13 23:17:29 +0530

    Added html template

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!+s+s7#ju(2yikjgc@mp+i&8+aqtr_m7^4$+-k1$)^--t$#^)5'
+SECRET_KEY =  '!+s+s7#ju(2yikjgc@mp+i&8+aqtr_m7^4$+-k1$)^--t$#^)5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
